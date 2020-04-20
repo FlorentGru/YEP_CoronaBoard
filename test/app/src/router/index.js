16756191d4d98215f1db9236270c6ea7b843f07e
Lucas Huet <lucashdg@hotmail.fr>
2020-04-20 14:15:13 +0200

    add login page done for first follow up

--- test/app/src/router/index.js
+++ test/app/src/router/index.js
@@ -1,29 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import LoginComponent from "../views/Login.vue"
+import SecureComponent from "../views/HomePage.vue"
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            redirect: {
+                name: "Login"
+            }
+        },
+        {
+            path: "/Login",
+            name: "Login",
+            component: LoginComponent
+        },
+        {
+            path: "/HomePage",
+            name: "HomePage",
+            component: SecureComponent
+        }
+    ]
 })
-
-export default router
---
+++ test/app/src/views/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="HomePage">
+        <h1>Home Page</h1>
+        <p>
+            You Are logged in
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomePage',
+        data() {
+            return {};
+        }
+    }
+</script>
+
+<style scoped>
+    #HomePage {
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
