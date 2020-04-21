eea969fb137b9adeb7971865ff84485a821c78ac
unknown <fabien.brosset@epitech.eu>
2020-04-21 14:36:46 +0200

    Map is displayed with some circle marker examples, need to work on the resizing of the circles when the user is zooming

---
+++ POC_Map/src/components/Maps.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <center><div id="map" class="map"></div></center>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Maps",
+        data: {
+            map: null,
+            tileLayer: null,
+            layers: [],
+            circle: null
+        },
+        mounted() {
+            this.initMap();
+            this.initLayers();
+        },
+        methods: {
+            initMap() {
+                this.map = L.map('map').setView([0, 0], 2);
+
+                this.tileLayer = L.tileLayer(
+                'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+                {
+                    maxZoom: 10,
+                    minZoom: 2,
+                    zoomDelta: 0.25,
+                    zoomSnap: 0.25,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+                }
+                );
+                this.tileLayer.addTo(this.map);
+            },
+            initLayers() {
+//" Province_State	Country_Region	Last_Update	            Lat	      Long_	               Confirmed	Deaths	Recovered	Active	Combined_Key"
+//"                 Poland	        2020-04-20 23:36:27	    51.9194	  19.1451	           9593	        380	    1133	    8080	  Poland"
+//" Quebec	        Canada	        2020-04-20 23:44:34	    52.9399	  -73.5491             19319	    939	    0	        18380	  Quebec, Canada"
+//"                 Spain	        2020-04-20 23:36:27	    40.463667 -3.74922	           200210	    20852	80587	    98771	  Spain"
+//"                 Portugal	    2020-04-20 23:36:27	    39.3999	  -8.2245	           20863	    735	    610	        19518	  Portugal"
+//"                 Namibia	        2020-04-20 23:36:27	    -22.9576  18.4904	           16	        0	    6	        10	      Namibia"
+                this.circle = L.circle([51.9194, 19.1451], {//Poland
+                  stroke: false,
+                  fillColor: 'red',
+                  fillOpacity: 0.8,
+                  radius: 9593 +200000,
+                }).addTo(this.map);
+                this.circle = L.circle([52.9399, -73.5491], {//Quebec
+                  stroke: false,
+                  fillColor: 'red',
+                  fillOpacity: 0.8,
+                  radius: 19319 +200000,
+                }).addTo(this.map);
+                this.circle = L.circle([40.463667, -3.74922], {//Spain
+                  stroke: false,
+                  fillColor: 'red',
+                  fillOpacity: 0.8,
+                  radius: 200210 +200000,
+                }).addTo(this.map);
+                this.circle = L.circle([39.3999, -8.2245], {//Portugal
+                  stroke: false,
+                  fillColor: 'red',
+                  fillOpacity: 0.8,
+                  radius: 20863 +200000,
+                }).addTo(this.map);
+                this.circle = L.circle([-22.9576, 18.4904], {//Namibia
+                  stroke: false,
+                  fillColor: 'red',
+                  fillOpacity: 0.8,
+                  radius: 16 +200000,
+                }).addTo(this.map);
+            },
+        }
+    }
+</script>
+
+<style scoped>
+.map { height: 800px; width: 1024px}
+</style>
