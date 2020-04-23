85fff12064a80340ff95979cdd59becaa953f8a3
Lucas Huet <lucashdg@hotmail.fr>
2020-04-23 14:32:49 +0200

    change test name in homepage

--- POC_Lucas/app/tests/unit/homepage.spec.js
+++ POC_Lucas/app/tests/unit/homepage.spec.js
@@ -3,7 +3,7 @@ import HomePage from '@/views/HomePage.vue'
 
 describe('HomePage.vue', () => {
     const wrapper = shallowMount(HomePage)
-    it('expect the render to contain a specific html', () => {
+    it('expect the render to contain a specific html conponent', () => {
       expect(wrapper.html()).toContain('<h1>HomePage</h1>')
     })
   })
