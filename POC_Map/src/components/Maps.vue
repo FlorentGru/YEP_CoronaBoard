234bc2bb452900d6f92e887ff4f9f87c82adff49
unknown <fabien.brosset@epitech.eu>
2020-04-27 16:55:49 +0200

    Place the circles with the coordinates gave in the json in assets folder, next step is merging to master

--- POC_Map/src/components/Maps.vue
+++ POC_Map/src/components/Maps.vue
@@ -5,13 +5,43 @@
 </template>
 
 <script>
+    import json from '../assets/countrycodes.json'
     export default {
         name: "Maps",
-        data: {
+        data () {
+            return {
             map: null,
             tileLayer: null,
             layers: [],
-            circle: null
+            circle: null,
+            countries: [
+                {
+                "Country": "ALA Aland Islands",
+                "CountryCode": "AX",
+                "Slug": "ala-aland-islands",
+                "NewConfirmed": 0,
+                "TotalConfirmed": 0,
+                "NewDeaths": 0,
+                "TotalDeaths": 0,
+                "NewRecovered": 0,
+                "TotalRecovered": 0,
+                "Date": "2020-04-05T06:37:00Z"
+                },
+                {
+                "Country": "Afghanistan",
+                "CountryCode": "AF",
+                "Slug": "afghanistan",
+                "NewConfirmed": 18,
+                "TotalConfirmed": 299,
+                "NewDeaths": 1,
+                "TotalDeaths": 7,
+                "NewRecovered": 0,
+                "TotalRecovered": 10,
+                "Date": "2020-04-05T06:37:00Z"
+                }
+                ],
+            coords: json
+            }
         },
         mounted() {
             this.initMap();
@@ -19,6 +49,7 @@
         },
         methods: {
             initMap() {
+
                 this.map = L.map('map', {renderer: L.svg()}).setView([0, 0], 2);
 
                 this.tileLayer = L.tileLayer(
@@ -32,114 +63,34 @@
                 }
                 );
                 this.tileLayer.addTo(this.map);
+
             },
+
             initLayers() {
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////"Province_State	Country_Region	Last_Update	            Lat	      Long_	               Confirmed	Deaths	Recovered	Active	  Combined_Key"////
-//////"                Poland	        2020-04-20 23:36:27	    51.9194	  19.1451	           9593	        380	    1133	    8080	  Poland"//////////
-//////"Quebec	        Canada	        2020-04-20 23:44:34	    52.9399	  -73.5491             19319	    939	    0	        18380	  Quebec, Canada"//
-//////"                Spain	        2020-04-20 23:36:27	    40.463667 -3.74922	           200210	    20852	80587	    98771	  Spain"///////////
-//////"                Portugal	        2020-04-20 23:36:27	    39.3999	  -8.2245	           20863	    735	    610	        19518	  Portugal"////////
-//////"                Namibia	        2020-04-20 23:36:27	    -22.9576  18.4904	           16	        0	    6	        10	      Namibia"/////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-                var Spain = {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [-3.74922, 40.463667]
+                this.countries.forEach(element => {
+                    if (this.coords[element.CountryCode.toLowerCase()]) {
+                    
+                        var dataCountry = {
+                            "type": "Feature",
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": [this.coords[element.CountryCode.toLowerCase()][1], this.coords[element.CountryCode.toLowerCase()][0]]
+                            }
+                        };
+                        L.geoJSON(dataCountry, {
+                            pointToLayer: function (feature, latlng) {
+                                return L.circleMarker(latlng, {
+                                    stroke: false,
+                                    radius: Math.log(element.TotalConfirmed),
+                                    fillColor: "red",
+                                    fillOpacity: 0.8
+                                }).bindPopup("<center>" + element.Country + "<br/>Confirmed Cases: " + element.TotalConfirmed + "<\center>");
+                            }
+                        }).addTo(this.map);
                     }
-                };
-                L.geoJSON(Spain, {
-                    pointToLayer: function (feature, latlng) {
-                        return L.circleMarker(latlng, {
-                            stroke: false,
-                            radius: Math.log(200210),
-                            fillColor: "red",
-                            fillOpacity: 0.8
-                        });
-                    }
-                }).addTo(this.map);
+                });
 
-
-
-                var Quebec = {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [-73.5491, 52.9399]
-                    }
-                };
-                L.geoJSON(Quebec, {
-                    pointToLayer: function (feature, latlng) {
-                        return L.circleMarker(latlng, {
-                            stroke: false,
-                            radius: Math.log(19319),
-                            fillColor: "red",
-                            fillOpacity: 0.8
-                        });
-                    }
-                }).addTo(this.map);
-
-
-
-                var Portugal = {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [-8.2245, 39.3999]
-                    }
-                };
-                L.geoJSON(Portugal, {
-                    pointToLayer: function (feature, latlng) {
-                        return L.circleMarker(latlng, {
-                            stroke: false,
-                            radius: Math.log(20863),
-                            fillColor: "red",
-                            fillOpacity: 0.8
-                        });
-                    }
-                }).addTo(this.map);
-
-
-
-                var Poland = {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [19.1451, 51.9194]
-                    }
-                };
-                L.geoJSON(Poland, {
-                    pointToLayer: function (feature, latlng) {
-                        return L.circleMarker(latlng, {
-                            stroke: false,
-                            radius: Math.log(9593),
-                            fillColor: "red",
-                            fillOpacity: 0.8
-                        });
-                    }
-                }).addTo(this.map);
-
-
-
-                var Namibie = {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [18.4904, -22.9576]
-                    }
-                };
-                L.geoJSON(Namibie, {
-                    pointToLayer: function (feature, latlng) {
-                        return L.circleMarker(latlng, {
-                            stroke: false,
-                            radius: Math.log(16),
-                            fillColor: "red",
-                            fillOpacity: 0.8
-                        });
-                    }
-                }).addTo(this.map);
             },
         }
     }
