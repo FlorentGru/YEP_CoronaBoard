ec8c219da5c165ca0879c887a7e0fac2c30e3b99
Lucas Huet <lucashdg@hotmail.fr>
2020-04-29 00:37:02 +0200

    add a few same test for other countries and a false country test for error management

--- POC_VueJs/tests/unit/data.spec.js
+++ POC_VueJs/tests/unit/data.spec.js
@@ -21,11 +21,48 @@ describe('Data.js', () => {
     const response = await api.default.getCountryData("France");
     const latest = response.data[response.data.length - 1]
 
-    // console.log(latest)
-
     expect(latest.Country).toBe("France")
     expect(typeof latest.Confirmed).toBe("number")
     expect(typeof latest.Deaths).toBe("number")
     expect(typeof latest.Recovered).toBe("number")
+  })
+
+  it('GetCountryData with Italy in argument', async () => {
+    const response = await api.default.getCountryData("Italy");
+    const latest = response.data[response.data.length - 1]
+
+    expect(latest.Country).toBe("Italy")
+    expect(typeof latest.Confirmed).toBe("number")
+    expect(typeof latest.Deaths).toBe("number")
+    expect(typeof latest.Recovered).toBe("number")
+  })
+
+  it('GetCountryData with China in argument', async () => {
+    const response = await api.default.getCountryData("China");
+    const latest = response.data[response.data.length - 1]
+
+    expect(latest.Country).toBe("China")
+    expect(typeof latest.Confirmed).toBe("number")
+    expect(typeof latest.Deaths).toBe("number")
+    expect(typeof latest.Recovered).toBe("number")
+  })
+
+  it('GetCountryData with India in argument', async () => {
+    const response = await api.default.getCountryData("India");
+    const latest = response.data[response.data.length - 1]
+
+    expect(latest.Country).toBe("India")
+    expect(typeof latest.Confirmed).toBe("number")
+    expect(typeof latest.Deaths).toBe("number")
+    expect(typeof latest.Recovered).toBe("number")
+  })
+
+  it('GetCountryData with invalid country in argument', async () => {
+    try {
+      await api.default.getCountryData("Country");
+    } catch (myerror) {
+      expect(myerror.data.status).toBe(404)
+      expect(myerror.data.message).toBe("Not Found")
+    }
   })
 })
